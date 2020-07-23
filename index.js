c7c7ecf83f53ceb0e17b40581ffc6ab2a0953d4b
kleytonsilvano <kleyton.silvano@gmail.com>
2020-07-22 23:40:26 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const funcoes = require("./meuModulo/funcoes")
+
+
+console.log("a) ",funcoes.adicionar(1,1));
+console.log("b) ",funcoes.adicionar(-123,123123));
+console.log("c) ",funcoes.multiplicar(8,0));
+console.log("d) ",funcoes.multiplicar(1239123,12313));
+console.log("e) ",funcoes.dividir(123,-12));
+console.log("f) ",funcoes.dividir(313123,0));
+
